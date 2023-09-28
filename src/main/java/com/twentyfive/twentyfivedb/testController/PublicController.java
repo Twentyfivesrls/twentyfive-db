18c64e42b5bd7f3b0319c6d7113cdd86544f1daf
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-09-28 13:32:28 +0200

    add test controller

--- src/main/java/com/twentyfive/twentyfivedb/testController/PublicController.java
+++ src/main/java/com/twentyfive/twentyfivedb/testController/PublicController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-public class TestController {
+public class PublicController {
 
     @GetMapping("/prova")
     public String prova(){
