20aefe3527151dbf1c74bd289df383825f64372f
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-09-28 13:19:01 +0200

    add test controller

---
+++ src/main/java/com/twentyfive/twentyfivedb/testController/TestController.java
@@ -0,0 +1,16 @@
+package com.twentyfive.twentyfivedb.testController;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/prova")
+    public String prova(){
+        return "prova";
+    }
+}
