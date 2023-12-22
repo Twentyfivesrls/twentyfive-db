112204bb09002dc06afe8965f14ecdac5bc25980
Raikozu <francescogaglianese93@gmail.com>
2023-12-22 15:20:26 +0100

    delete partenadpp-db

--- src/main/java/com/twentyfive/twentyfivedb/partenappDB/repositories/QuotazioneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.twentyfive.twentyfivedb.partenappDB.repositories;
-
-import com.twentyfive.twentyfivemodel.models.partenupModels.QuotazioneGiornaliera;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuotazioneRepository extends JpaRepository<QuotazioneGiornaliera, Long> {
-}
--- src/main/java/com/twentyfive/twentyfivedb/partenappDB/repositories/VoceDiRettificaConValoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.twentyfive.twentyfivedb.partenappDB.repositories;
-
-import com.twentyfive.twentyfivemodel.models.partenupModels.VoceDiRettificaConValore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoceDiRettificaConValoreRepository extends JpaRepository<VoceDiRettificaConValore,Long> {
-}
