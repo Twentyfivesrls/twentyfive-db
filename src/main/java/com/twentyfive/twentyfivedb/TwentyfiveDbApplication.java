fce4203a62e99832be5cf58b1a139fa322dfa314
Raikozu <francescogaglianese93@gmail.com>
2024-01-16 16:40:32 +0100

    Excel e DbApplication fixati

--- src/main/java/com/twentyfive/twentyfivedb/TwentyfiveDbApplication.java
+++ src/main/java/com/twentyfive/twentyfivedb/TwentyfiveDbApplication.java
@@ -3,9 +3,10 @@ package com.twentyfive.twentyfivedb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EntityScan("com.twentyfive.twentyfivemodel.models.*")
 public class TwentyfiveDbApplication {
 
